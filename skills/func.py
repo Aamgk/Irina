2f5e9068194d663c4d57536c039f8f0de1f205f9
Aamgk <osiilen@bk.ru>
2023-08-11 16:55:21 -0500

    skill telling functions

---
+++ skills/func.py
@@ -0,0 +1,4 @@
+class Functions:
+    def tell_Functions(self):
+        Functionalities = "Я умею: говорить время и дату в настоящий момент; погоду и давление любого города; открывать сайты как Ютуб, ВКонтакте и Гугл"
+        return Functionalities
